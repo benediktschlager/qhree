95b79ebedfb87c04dec6f95b9debb51c25201fe3
Benedikt Schlager <benedikt.schlager@protonmail.com>
2024-05-21 07:50:53 +0200

    initial commit

---
+++ backend/src/test/java/com/github/benediktschlager/qhree/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.benediktschlager.qhree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
